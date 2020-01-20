8ff9b435f7232c670f9d711198c7c5831036b231
Luke Williams <luke@iquidus.io>
2020-01-20 02:29:07 +0100

    components: add Bar chart

---
+++ components/charts/Bar.js
@@ -0,0 +1,17 @@
+import { HorizontalBar, mixins } from 'vue-chartjs'
+const { reactiveProp } = mixins
+export default {
+  extends: HorizontalBar,
+  mixins: [reactiveProp],
+  props: {
+    options: {
+      type: Object,
+      default() {
+        return {}
+      }
+    }
+  },
+  mounted() {
+    this.renderChart(this.chartData, this.options)
+  }
+}
---
+++ components/charts/Bar.vue
@@ -0,0 +1,84 @@
+<template>
+  <v-card class="mr-1" style="background-color:rgba(0,0,0,0);" flat>
+    <client-only placeholder="Loading...">
+      <bar-chart
+        :chart-data="data"
+        :options="options"
+        :height="300"
+        :styles="{ height: '300px' }"
+      />
+    </client-only>
+  </v-card>
+</template>
+
+<script>
+import BarChart from './Bar.js'
+
+export default {
+  components: {
+    BarChart
+  },
+  props: {
+    title: {
+      type: String,
+      default() {
+        return ''
+      }
+    },
+    data: {
+      type: Object,
+      default() {
+        return {}
+      }
+    },
+    left: {
+      type: Boolean,
+      default() {
+        return true
+      }
+    },
+    right: {
+      type: Boolean,
+      default() {
+        return false
+      }
+    }
+  },
+  computed: {
+    options() {
+      return {
+        responsive: true,
+        maintainAspectRatio: false,
+        title: {
+          display: false,
+          position: 'top',
+          text: this.title
+        },
+        scales: {
+          xAxes: [
+            {
+              display: false,
+              ticks: {
+                beginAtZero: true
+              }
+            }
+          ],
+          yAxes: [
+            {
+              display: true
+            }
+          ]
+        },
+        legend: {
+          display: false,
+          position: 'top',
+          align: 'start',
+          labels: {
+            fontFamily: "'Avenir', 'Helvetica', 'Arial', 'sans-serif'"
+          }
+        }
+      }
+    }
+  }
+}
+</script>
