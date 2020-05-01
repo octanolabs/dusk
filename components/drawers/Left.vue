4ee1e9d6ee590d59c2b1265fb8a4aad7443452d6
Luke Williams <luke@iquidus.io>
2020-05-01 16:01:30 +0200

    Left: add menu hover tooltips

--- components/drawers/Left.vue
+++ components/drawers/Left.vue
@@ -1,27 +1,42 @@
 <template>
   <v-navigation-drawer left app fixed mini-variant>
     <v-list nav dense>
-      <v-list-item link>
-        <v-list-item-icon>
-          <v-icon>mdi-server</v-icon>
-        </v-list-item-icon>
-        <v-list-item-title>{{ $t('menu.server') }}</v-list-item-title>
-      </v-list-item>
+      <v-tooltip right>
+        <template v-slot:activator="{ on }">
+          <v-list-item link v-on="on">
+            <v-list-item-icon>
+              <v-icon>mdi-server</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>{{ $t('menu.server') }}</v-list-item-title>
+          </v-list-item>
+        </template>
+        <span>{{ $t('menu.server') }}</span>
+      </v-tooltip>
     </v-list>
     <template v-slot:append>
       <v-list nav dense>
-        <v-list-item link>
-          <v-list-item-icon>
-            <v-icon>mdi-account-cog</v-icon>
-          </v-list-item-icon>
-          <v-list-item-title>{{ $t('menu.settings') }}</v-list-item-title>
-        </v-list-item>
-        <v-list-item link @click.stop="logout()">
-          <v-list-item-icon>
-            <v-icon>mdi-account-lock</v-icon>
-          </v-list-item-icon>
-          <v-list-item-title>{{ $t('menu.logout') }}</v-list-item-title>
-        </v-list-item>
+        <v-tooltip right>
+          <template v-slot:activator="{ on }">
+            <v-list-item link v-on="on">
+              <v-list-item-icon>
+                <v-icon>mdi-account-cog</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>{{ $t('menu.settings') }}</v-list-item-title>
+            </v-list-item>
+          </template>
+          <span>{{ $t('menu.settings') }}</span>
+        </v-tooltip>
+        <v-tooltip right>
+          <template v-slot:activator="{ on }">
+            <v-list-item link v-on="on" @click.stop="logout()">
+              <v-list-item-icon>
+                <v-icon>mdi-account-lock</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>{{ $t('menu.logout') }}</v-list-item-title>
+            </v-list-item>
+          </template>
+          <span>{{ $t('menu.logout') }}</span>
+        </v-tooltip>
       </v-list>
     </template>
   </v-navigation-drawer>
