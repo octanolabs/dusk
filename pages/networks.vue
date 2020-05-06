e0f901c29437a6a3e057472b5d2139dfc94cd68e
Luke Williams <luke@iquidus.io>
2020-05-06 16:03:40 +0200

    add clients, networks, packages, page skeletons

---
+++ pages/networks.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <v-tabs>
+      <v-tab :key="0">Networks</v-tab>
+      <v-tab-item :key="0">
+        <v-card>
+          <v-card-text>
+            List networks here
+          </v-card-text>
+        </v-card>
+      </v-tab-item>
+    </v-tabs>
+  </v-container>
+</template>
+
+<script>
+export default {
+  middleware: 'auth'
+}
+</script>
