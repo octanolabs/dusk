41ca7946f34368c505baf655c8c49c466f849797
Luke Williams <luke@iquidus.io>
2020-05-08 15:14:02 +0200

    packages: refactor provider, parse client data, check remote source use local as fallback

--- provider/packages.js
+++ provider/packages.js
@@ -1,48 +1,147 @@
+/*
+ * Dusk package provider
+ */
+import axios from 'axios'
 import jf from 'jsonfile'
 import fs from 'fs'
+import os from 'os'
 import { promisify } from 'util'
 import consola from 'consola'
 import path from 'path'
 
+// promisify fs functions so we can async/await them later.
 const stat = promisify(fs.stat)
 const readdir = promisify(fs.readdir)
+const readJson = promisify(jf.readFile)
 
+// caches
 let PACKAGES = []
 let CUSTOM = []
+let CLIENTS = []
+
+const platform = async function() {
+  try {
+    let arch = await os.arch()
+    const platform = await os.platform()
+    if (arch === 'x64') {
+      arch = 'amd64'
+    }
+    return platform + '-' + arch
+  } catch (e) {
+    consola.error(new Error(e))
+  }
+}
+
+const loadPackages = async function(pkgs) {
+  try {
+    const ls = await readdir(pkgs)
+    for (let x of ls) {
+      const packagePath = path.join(pkgs, x)
+      const duskpkg = await stat(packagePath)
+      if (duskpkg.isDirectory()) {
+        let pkg = await readJson(path.join(packagePath, 'dusk.json'))
+        PACKAGES.push(pkg)
+        if (pkg.client) {
+          const clientData = await getPackageData (
+            path.join(packagePath, pkg.client.local),
+            'https://github.com/' + pkg.client.remote
+          )
+          const client = await parseClient(clientData)
+          CLIENTS.push(client)
+        }
+        return
+      } else {
+        consola.error('path not found: ' + packagePath)
+      }
+    }
+  } catch (e) {
+    consola.error(new Error(e))
+  }
+}
+
+const parseClient = async function(json) {
+  try {
+    let client = json
+    const build = await platform()
+    client.platform = build
+    client.downloaded = 0
+    let releases = []
+    for(let y in client.releases) {
+      let release = client.releases[y]
+      if (release[build]){
+        releases.push({
+          version: release.version,
+          status: 0,
+          maxHeight: release.maxHeight,
+          tag: release.tag,
+          note: release.note,
+          download: release[build]
+        })
+      }
+    }
+    client.releases = releases
+    return client
+  } catch (e) {
+    consola.error(new Error(e))
+    return
+  }
+}
+
+/* getPackageData
+ * fetch data from remote json or use local copy as a fallback
+ * injects source (remote/local) and returns json data as object
+ */
+const getPackageData = async function(localPath, remotePath) {
+  try {
+    let remote = await axios.get(remotePath)
+    if (remote && remote.data) { remote.data.source = 'remote' }
+    return remote.data || {}
+  } catch (e) {
+    try {
+      const localJson = await stat(localPath)
+      if (localJson.isFile()) {
+        let local = await readJson(localPath)
+        if (local) { local.source = 'local' }
+        return local || {}
+      } else {
+        consola.error('localPath is not a file (expected json): ' + localPath)
+      }
+    } catch (e) {
+      consola.error(new Error(e))
+      return
+    }
+  }
+}
 
 export default {
+  // clear all caches
   clear() {
     PACKAGES = [],
-    CUSTOM = []
+    CUSTOM = [],
+    CLIENTS = []
   },
+  // return caches
   get() {
-    return { octano: PACKAGES, custom: CUSTOM }
+    return {
+      octano: PACKAGES,
+      custom: CUSTOM,
+      clients: CLIENTS
+    }
   },
+  // set caches
   async set(rootPath) { // './packages'
     try {
-      // check path exists and is dir
-      const pathExists = await stat(rootPath)
-      if (pathExists.isDirectory()) {
-        const octanoDir = path.join(rootPath, 'octano')
-        const customDir = path.join(rootPath, 'custom')
-        const octanoExists = await stat(octanoDir)
-        const customExists = await stat(customDir)
-        if (octanoExists.isDirectory()) {
-          // ls contents
-          const ls = await readdir(octanoDir)
-          for (let x of ls) {
-            const packagePath = path.join(octanoDir, x)
-            const duskpkg = await stat(packagePath)
-            if (duskpkg.isDirectory()) {
-              jf.readFile(path.join(packagePath, 'dusk.json'), function (err, local) {
-                if (err) {
-                  consola.error(new Error(err))
-                } else {
-                  PACKAGES.push(local)
-                }
-              })
-            }
-          }
+      // check packages (rootPath) directory exists
+      const pkgs = await stat(rootPath)
+      if (pkgs.isDirectory()) {
+        // set paths
+        const octanoPath = path.join(rootPath, 'octano')
+        const customPath = path.join(rootPath, 'custom')
+        // check packages/octano directory exists
+        const octano = await stat(octanoPath)
+        if (octano.isDirectory()) {
+          // load packages
+          await loadPackages(octanoPath)
         } else {
           consola.error('octano packages path not found: ' + rootPath)
         }
