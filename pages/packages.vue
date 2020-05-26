8576fe057919d52a9fab37b4d1543f9b5ad52f25
Luke Williams <luke@iquidus.io>
2020-05-26 20:50:40 +0200

    packages: add breadcrumbs

--- pages/packages.vue
+++ pages/packages.vue
@@ -1,7 +1,15 @@
 <template>
   <v-container>
+    <v-row>
+      <v-breadcrumbs :items="breadcrumbs"></v-breadcrumbs>
+    </v-row>
+    <v-alert type="info">
+      Packages enable support for new networks, clients and interfaces. By
+      default Dusk includes a number of packages, however you can easily add
+      your own custom packages for additional support.
+    </v-alert>
     <v-tabs>
-      <v-tab :key="0">Packages</v-tab>
+      <v-tab :key="0">Default</v-tab>
       <v-tab :key="1">Custom</v-tab>
       <v-tab-item :key="0">
         <v-card>
@@ -28,6 +36,22 @@ export default {
   middleware: 'auth',
   components: {
     PackagesTable
+  },
+  data() {
+    return {
+      breadcrumbs: [
+        {
+          text: 'Dusk',
+          disabled: false,
+          to: '/'
+        },
+        {
+          text: 'Packages',
+          disabled: true,
+          to: '/packages'
+        }
+      ]
+    }
   },
   computed: {
     packages() {
