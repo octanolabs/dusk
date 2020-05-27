ee0b9ba3eacc067348b07957f193185222da4199
Luke Williams <luke@iquidus.io>
2020-05-27 12:34:26 +0200

    Create: select a client, filterClientsByNetwork(), grayscale unselected

--- pages/create.vue
+++ pages/create.vue
@@ -30,9 +30,14 @@
             style="display:inline-block;"
             hover
             ripple
-            :raise="selectedNetwork == network.networkId"
-            class="mx-1"
-            @click.stop="selectedNetwork = network.networkId"
+            :raised="selectedNetwork === network.networkId"
+            class="ma-1"
+            @click.stop="
+              selectedNetwork = network.networkId
+              selectedNetworkType = 'mainnet'
+              selectedClient = false
+              filterClientsByNetwork()
+            "
           >
             <v-avatar tile color="#111" size="180">
               <img
@@ -41,6 +46,10 @@
                 "
                 height="140"
                 style="max-height:140px"
+                :class="{
+                  grayscale:
+                    selectedNetwork && selectedNetwork !== network.networkId
+                }"
               />
             </v-avatar>
             <v-chip
@@ -75,9 +84,14 @@
                   style="min-width:182px;display:inline-block;"
                   hover
                   ripple
-                  :raise="selectedNetwork == network.networkId"
-                  class="mx-1"
-                  @click.stop="selectedNetwork = network.networkId"
+                  :raised="selectedNetwork === network.networkId"
+                  class="ma-1"
+                  @click.stop="
+                    selectedNetwork = network.networkId
+                    selectedNetworkType = 'testnet'
+                    selectedClient = false
+                    filterClientsByNetwork()
+                  "
                 >
                   <v-avatar tile color="#111" size="180">
                     <img
@@ -88,6 +102,11 @@
                       "
                       height="140"
                       style="max-height:140px"
+                      :class="{
+                        grayscale:
+                          selectedNetwork &&
+                          selectedNetwork !== network.networkId
+                      }"
                     />
                   </v-avatar>
                   <v-chip
@@ -116,6 +135,44 @@
         </v-row>
       </v-card-text>
     </v-card>
+    <v-flex shrink>
+      <v-slide-y-transition>
+        <v-card v-show="!!selectedNetwork" class="px-1 py-2 mt-2">
+          <v-card-title class="py-1">
+            <v-icon class="mx-1">mdi-cloud</v-icon>
+            Select a client
+          </v-card-title>
+          <v-card-text>
+            <v-row no-gutters>
+              <v-card
+                v-for="client in availableClients"
+                :key="client.name"
+                outlined
+                style="display:inline-block;"
+                hover
+                ripple
+                :raised="selectedClient === client.name"
+                class="ma-1"
+                @click.stop="selectedClient = client.name"
+              >
+                <v-avatar tile color="#111" size="180">
+                  <img
+                    :src="
+                      require('~/packages' + client.duskpkg.path + client.icon)
+                    "
+                    height="140"
+                    style="max-height:140px"
+                  />
+                </v-avatar>
+                <v-card-text class="text-center">
+                  {{ client.name }}
+                </v-card-text>
+              </v-card>
+            </v-row>
+          </v-card-text>
+        </v-card>
+      </v-slide-y-transition>
+    </v-flex>
   </v-container>
 </template>
 
@@ -126,6 +183,9 @@ export default {
     return {
       showTestnets: false,
       selectedNetwork: false,
+      selectedNetworkType: 'mainnet',
+      selectedClient: false,
+      availableClients: [],
       breadcrumbs: [
         {
           text: 'Instances',
@@ -146,6 +206,25 @@ export default {
     },
     testnets() {
       return this.$store.state.packages.networks.testnet || []
+    },
+    clients() {
+      return this.$store.state.packages.clients || []
+    }
+  },
+  methods: {
+    filterClientsByNetwork() {
+      const network = this.$store.state.packages.networks[
+        this.selectedNetworkType
+      ][this.selectedNetwork]
+      const supportedClients = network.clients
+      this.availableClients = []
+      for (const n in supportedClients) {
+        for (const c in this.clients) {
+          if (supportedClients[n] === this.clients[c].name) {
+            this.availableClients.push(this.clients[c])
+          }
+        }
+      }
     }
   }
 }
