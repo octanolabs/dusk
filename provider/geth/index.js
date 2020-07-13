4ac7701ef0c230c8765c521fddc9542fe7ed82ce
Luke Williams <luke@iquidus.io>
2020-07-13 17:05:15 +0200

    add GethProvider

--- provider/geth/index.js
+++ provider/geth/index.js
@@ -5,63 +5,70 @@ import consola from 'consola'
 import NanoTimer from 'nanotimer'
 
 import BlockCache from './sub/blocks.js'
-// import PeerCache from './sub/nodes.js'
+import PeerCache from './sub/peers.js'
 import Loop from '../lib/loop.js'
 
 const providers = {}
 
-const GethProvider = function(id, ipcPath, interval) {
-  this.ipcPath = ipcPath
-  this.id = id
-  this.web3 = null
-  this.web3Admin = null
-  this.timer = new NanoTimer()
-  this.interval = interval || '1s'
-  this.blockCache = BlockCache.new(100) // 100 blocks
-  // this.peerCache = PeerCache.new(86400) // 1 day
+class Provider {
+  constructor(id, interval) {
+    this.id = id
+    this.interval = interval
+    this.timer = new NanoTimer()
+  }
+}
+
+class GethProvider extends Provider {
+  constructor(ipcPath, id, interval) {
+    super(id, interval)
+    this.ipcPath = ipcPath
+    this.web3 = null
+    this.web3Admin = null
+    this.blockCache = BlockCache.new(100) // 100 blocks
+    this.peerCache = PeerCache.new(86400) // 1day
+  }
+  async connect() {
+    try {
+      this.web3 = await new Web3(this.ipcPath, net)
+      this.web3Admin = await new Admin(this.ipcPath, net)
+    } catch(err) {
+      consola.error(new Error(err))
+    }
+  }
+  setBlocks(cb) {
+    const self = this
+    this.web3.eth.getBlock('pending', false, function(err, head) {
+      if (err || !head) {
+        consola.fatal(new Error(err))
+        return cb()
+      } else {
+        const cache = []
+        const blockNumber = head.number - geth.blockCache.maxlen * 2
+        Loop.sync(self.blockCache.maxlen * 2 + 1, function(loop) {
+          const i = loop.iteration()
+          self.web3.eth.getBlock(blockNumber + i, false, function(err, block) {
+            if (!err && block) {
+              cache.push(block)
+              loop.next()
+            } else {
+              loop.break(true)
+              loop.next()
+            }
+          })
+        },
+        function() {
+          self.blockCache.set(cache, function() {
+            return cb()
+          })
+        })
+      }
+    })
+  }
+
 }
 
 export default {
-  getProvider(id) {
-    return providers[id] || null
-  },
-  setProvider(id, ipcPath, interval, cb) {
-    if (providers.id) {
-      // provider exists, update
-    } else {
-      // new provider, init
-      const geth = new GethProvider(id, ipcPath, interval)
-      geth.web3 = await new Web3(ipcPath, net)
-      geth.web3Admin = await new Admin(ipcPath, net)
-      geth.web3.eth.getBlock('pending', false, function(err, head) {
-        if (err || !head) {
-          consola.fatal(new Error(err))
-          return cb()
-        } else {
-          const cache = []
-          const blockNumber = head.number - geth.blockCache.maxlen * 2
-          Loop.sync(
-            geth.blockCache.maxlen * 2 + 1,
-            function(loop) {
-              const i = loop.iteration()
-              geth.web3.eth.getBlock(blockNumber + i, false, function(err, block) {
-                if (!err && block) {
-                  cache.push(block)
-                  loop.next()
-                } else {
-                  loop.break(true)
-                  loop.next()
-                }
-              })
-            },
-            function() {
-              geth.blockCache.set(cache, function() {
-                return cb(providers)
-              })
-            }
-          )
-        }
-      }
-    }
+  new(ipcPath, instanceId, pollingInterval) {
+    return new GethProvider(ipcPath, instanceId, pollingInterval)
   }
 }
