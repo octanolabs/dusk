e926c760747532fa0df691f1296cc3d697aa2b26
Luke Williams <luke@iquidus.io>
2020-08-11 19:57:51 +0200

    nuxt.config: its cookie time

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,7 @@ module.exports = {
       secret: 'super-secret-key',
       resave: false,
       saveUninitialized: false,
-      cookie: { maxAge: 60000 }
+      cookie: { maxAge: false }
     }),
     // Api middleware
     '~/api'
